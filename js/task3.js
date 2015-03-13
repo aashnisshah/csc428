9e307af613e6a6cbc9ba33e217fe0ad98506e367
Aashni Shah <contact@aashni.me>
2015-03-13 02:38:48 -0400

    adding more tests and numbers to know what the order is for the imag tracking

--- js/task3.js
+++ js/task3.js
@@ -7,7 +7,7 @@ var offsetX = 0;
 var offsetY = 0;
 var backgroundImage = "../img/background/task1.png";
 var PART = 1;
-var TOTALPARTS = 1;
+var TOTALPARTS = 4;
 var TRACK = false;
 var ATTEMPT = 1;
 
@@ -37,6 +37,117 @@ var circle = {
 			"radius": 10,
 		},
 	},
+	"2": {
+		"parts": 4,
+		"first": 1,
+		"last": 1,
+		"1": {
+			"startX": 350,
+			"startY": 250,
+			"radius": 10,
+		},
+		"2": {
+			"startX": 650,
+			"startY": 250,
+			"radius": 10,
+		},
+		"3": {
+			"startX": 550,
+			"startY": 450,
+			"radius": 10,
+		},
+		"4": {
+			"startX": 250,
+			"startY": 450,
+			"radius": 10,
+		},
+	},
+	"3": {
+		"parts": 5,
+		"first": 1,
+		"last": 1,
+		"1": {
+			"startX": 250,
+			"startY": 550,
+			"radius": 10,
+		},
+		"2": {
+			"startX": 400,
+			"startY": 250,
+			"radius": 10,
+		},
+		"3": {
+			"startX": 550,
+			"startY": 550,
+			"radius": 10,
+		},
+		"4": {
+			"startX": 250,
+			"startY": 350,
+			"radius": 10,
+		},
+		"5": {
+			"startX": 550,
+			"startY": 350,
+			"radius": 10,
+		},
+	},
+	"4": {
+		"parts": 10,
+		"first": 1,
+		"last": 1,
+		"1": {
+			"startX": 250,
+			"startY": 550,
+			"radius": 10,
+		},
+		"2": {
+			"startX": 450,
+			"startY": 150,
+			"radius": 10,
+		},
+		"3": {
+			"startX": 550,
+			"startY": 350,
+			"radius": 10,
+		},
+		"4": {
+			"startX": 450,
+			"startY": 450,
+			"radius": 10,
+		},
+		"5": {
+			"startX": 550,
+			"startY": 500,
+			"radius": 10,
+		},
+		"6": {
+			"startX": 350,
+			"startY": 280,
+			"radius": 10,
+		},
+		"7": {
+			"startX": 150,
+			"startY": 150,
+			"radius": 10,
+		},
+		"8": {
+			"startX": 200,
+			"startY": 450,
+			"radius": 10,
+		},
+		"9": {
+			"startX": 430,
+			"startY": 350,
+			"radius": 10,
+		},
+		"10": {
+			"startX": 120,
+			"startY": 200,
+			"radius": 10,
+		},
+	},
+	
 }
 
 window.onload = function() {};
@@ -63,9 +174,11 @@ function draw() {
 			5
 		);
 		drawCircle(circle[PART][i].startX, circle[PART][i].startY, circle[PART][i].radius);
+  		context.strokeText('' + i, circle[PART][i].startX - 4, circle[PART][i].startY + 4);
 	}
 
 	drawCircle(circle[PART]["1"].startX, circle[PART]["1"].startY, circle[PART]["1"].radius);
+  	context.strokeText('1', circle[PART]["1"].startX - 4, circle[PART]["1"].startY + 4);
 	
 }
 
@@ -77,9 +190,10 @@ function drawCircle(startX, startY, radius) {
 	context.arc(startX, startY, radius, 0, 2 * Math.PI, false);
 	context.fillStyle = 'purple';
 	context.fill();
-	context.lineWidth = 3;
-	context.strokeStyle = '#000000';
+	context.lineWidth = 1;
+	context.strokeStyle = '#e4e4e4';
 	context.stroke();
+	context.font="10px verdana";
 }
 
 /**
